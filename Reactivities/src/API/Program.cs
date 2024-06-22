154b560944ef287af47d32bea7055ee3222671a0
juanarayafonsec <juan-jose95@hotmail.com>
2024-06-22 14:25:38 +0200

    Default Controller Removed

--- Reactivities/src/API/Program.cs
+++ Reactivities/src/API/Program.cs
@@ -23,26 +23,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 using var scope = app.Services.CreateAsyncScope();
 var services = scope.ServiceProvider;
 try
@@ -57,8 +37,3 @@ catch (Exception ex)
     logger.LogError(ex,"Something went wrong during db migration");
 }
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
