258b05f91b793aa51a1611bd4b27224383f16fbc
juanarayafonsec <juan-jose95@hotmail.com>
2024-06-30 21:41:04 +0200

    First controller added

--- Reactivities/src/API/Controllers/ActivitiesController.cs
+++ Reactivities/src/API/Controllers/ActivitiesController.cs
@@ -1,13 +1,22 @@
-﻿using Asp.Versioning;
+﻿using Application.Queries;
+using Asp.Versioning;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
 [ApiVersion(1)]
 public class ActivitiesController : BaseApiController
 {
-    public ActivitiesController()
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        
+        return await Mediator.Send(new GetActivitiesQuery());
     }
 
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivity(Guid id)
+    {
+        return await Mediator.Send(new GetActivityQuery { Id = id });
+    }
 }
