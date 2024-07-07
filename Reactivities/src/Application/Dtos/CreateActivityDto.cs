4cd457f536a8f4e560dda9f92c4a2618b06a9c1e
juanarayafonsec <juan-jose95@hotmail.com>
2024-07-07 11:42:05 +0200

    Dtos

---
+++ Reactivities/src/Application/Dtos/CreateActivityDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Dtos;
+public class CreateActivityDto
+{
+    public string Title { get; set; }
+    public DateTime Date { get; set; }
+    public string Description { get; set; }
+    public string Category { get; set; }
+    public string City { get; set; }
+    public string Venue { get; set; }
+}
