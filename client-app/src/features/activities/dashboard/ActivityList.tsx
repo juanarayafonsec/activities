8d2923d68364d44bd50b5e515364275b82daf61d
juanarayafonsec <juan-jose95@hotmail.com>
2024-07-30 10:50:40 +0200

    FE activities style improvement

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,63 +1,28 @@
-import {
-  Button,
-  Item,
-  ItemContent,
-  ItemDescription,
-  ItemExtra,
-  ItemGroup,
-  ItemHeader,
-  ItemMeta,
-  Label,
-  Segment,
-} from "semantic-ui-react";
-import { SyntheticEvent, useState } from "react";
+import { Header, ItemGroup, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
-import { Link } from "react-router-dom";
-
+import { Fragment } from "react/jsx-runtime";
+import ActivityListItem from "./ActivityListItem";
 export default observer(function ActivityList() {
   const { activityStore } = useStore();
-  const { deleteActivity, activitiesByDate, loading } = activityStore;
-  const [target, setTarget] = useState("");
-
-  function handleActivityDelete(
-    e: SyntheticEvent<HTMLButtonElement>,
-    id: string
-  ) {
-    setTarget(e.currentTarget.name);
-    deleteActivity(id);
-  }
+  const { groupedActivities } = activityStore;
 
   return (
-    <Segment>
-      <ItemGroup divided>
-        {activitiesByDate.map((activity) => (
-          <Item key={activity.id}>
-            <ItemContent>
-              <ItemHeader as={"a"}>{activity.title}</ItemHeader>
-              <ItemMeta>{activity.date}</ItemMeta>
-              <ItemDescription>
-                <div>{activity.description}</div>
-                <div>
-                  {activity.city}, {activity.venue}
-                </div>
-              </ItemDescription>
-              <ItemExtra>
-                <Button as={Link} to={`/activities/${activity.id}`} floated="right" content="View" color="blue"/>
-                <Button
-                  name={activity.id}
-                  loading={loading && target === activity.id}
-                  onClick={(e) => handleActivityDelete(e, activity.id)}
-                  floated="right"
-                  content="Delete"
-                  color="red"
-                />
-                <Label basic content={activity.category} />
-              </ItemExtra>
-            </ItemContent>
-          </Item>
-        ))}
-      </ItemGroup>
-    </Segment>
+    <>
+      {groupedActivities.map(([group, activities]) => (
+        <Fragment key={group}>
+          <Header sub color="teal">
+            {group}
+          </Header>
+          <Segment>
+            <ItemGroup divided>
+              {activities.map(activity => (
+                <ActivityListItem key={activity.id} activity={activity} />
+              ))}
+            </ItemGroup>
+          </Segment>
+        </Fragment>
+      ))}
+    </>
   );
 });
---
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -0,0 +1,68 @@
+import { Link } from "react-router-dom";
+import {
+  Button,
+  Icon,
+  Item,
+  ItemContent,
+  ItemDescription,
+  ItemGroup,
+  ItemHeader,
+  ItemImage,
+  Segment,
+  SegmentGroup,
+} from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+import { useStore } from "../../../app/stores/store";
+import { SyntheticEvent, useState } from "react";
+
+interface Props {
+  activity: Activity;
+}
+export default function ActivityLisyItem({ activity }: Props) {
+  const { activityStore } = useStore();
+  const { deleteActivity, loading } = activityStore;
+
+  const [target, setTarget] = useState("");
+
+  function handleActivityDelete(
+    e: SyntheticEvent<HTMLButtonElement>,
+    id: string
+  ) {
+    setTarget(e.currentTarget.name);
+    deleteActivity(id);
+  }
+  return (
+    <SegmentGroup>
+      <Segment>
+        <ItemGroup>
+          <Item>
+            <ItemImage size="tiny" circular src="assets/user.png" />
+            <ItemContent>
+              <ItemHeader as={Link} to={`/activities/${activity.id}`}>
+                {activity.title}
+              </ItemHeader>
+              <ItemDescription>Hpsted by Bob</ItemDescription>
+            </ItemContent>
+          </Item>
+        </ItemGroup>
+      </Segment>
+      <Segment>
+        <span>
+          <Icon name="clock" /> {activity.date}
+          <Icon name="marker" /> {activity.venue}
+        </span>
+      </Segment>
+      <Segment secondary>Attendess go here</Segment>
+      <Segment clearing>
+        <span> {activity.description}</span>
+        <Button
+          as={Link}
+          to={`/activities/${activity.id}`}
+          color="teal"
+          floated="right"
+          content="View"
+        />
+      </Segment>
+    </SegmentGroup>
+  );
+}
