8afe2c137f778b6575c06d2dcddc020af1e1d45e
juanarayafonsec <juan-jose95@hotmail.com>
2024-09-29 17:20:17 +0200

    Add identity and update EF

--- Reactivities/src/Persistence/Context/DataContext.cs
+++ Reactivities/src/Persistence/Context/DataContext.cs
@@ -1,8 +1,9 @@
 using Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence.Context;
-public class DataContext(DbContextOptions options) : DbContext(options)
+public class DataContext(DbContextOptions options) : IdentityDbContext<AppUser>(options)
 {
     public DbSet<Activity> Activities { get; set; }
 }
