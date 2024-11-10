4288bf2959c46e9ead9e5a2072226683cbb86ecf
juanarayafonsec <juan-jose95@hotmail.com>
2024-11-10 21:48:29 +0100

    Add Photo controller and fix photo crop type

--- Reactivities/src/Infrastructure/Photos/PhotoAccessor.cs
+++ Reactivities/src/Infrastructure/Photos/PhotoAccessor.cs
@@ -26,7 +26,7 @@ public class PhotoAccessor : IPhotoAccessor
         var uploadParams = new ImageUploadParams
         {
             File = new FileDescription(file.FileName, stream),
-            Transformation = new Transformation().Height(500).Width(500).Crop("limit")
+            Transformation = new Transformation().Height(500).Width(500).Crop("fill")
         };
 
         var uploadResult = await _cloudinary.UploadAsync(uploadParams);
