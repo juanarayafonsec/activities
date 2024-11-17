0f1d7db97084bbc4fd6ca73fdb6bb9de12e40330
juanarayafonsec <juan-jose95@hotmail.com>
2024-11-17 20:31:56 +0100

    add upload widget ui

--- client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
+++ client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
@@ -1,14 +1,21 @@
-import { Grid, GridColumn, Header } from "semantic-ui-react";
+import { Grid, GridColumn, Header, Image } from "semantic-ui-react";
+import PhotosWidgetDropzone from "./PhotosWidgetDropzone";
+import { useState } from "react";
 
 export default function PhotoUploadWidget() {
+    const [files, setFiles] = useState<any>([]);
   return (
     <Grid>
       <GridColumn width={4}>
         <Header color="teal" content="Step 1 - Add Photo" />
+        <PhotosWidgetDropzone setFiles={setFiles}/>
       </GridColumn>
       <GridColumn width={1}/>
       <GridColumn width={4}>
         <Header color="teal" content="Step 2 - Resize image" />
+        {files && files.length > 0 && (
+            <Image src={files[0].preview}/>
+        )}
       </GridColumn>
       <GridColumn width={1}/>
       <GridColumn width={4}>
