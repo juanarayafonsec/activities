982e4c9c865e40d43cc09a47c42385061cce801c
juanarayafonsec <juan-jose95@hotmail.com>
2025-07-06 00:18:03 +0200

    Add ActivityContext and seed initial data
    Updated `Program.cs` to include Entity Framework Core namespaces and configure `ActivityContext` with SQLite. Introduced migration and seeding process for database initialization.
    
    Created/modified `DbInitializer.cs` to implement `SeedData` method, which populates the database with predefined activities if none exist.

--- src/Activities.Api/Program.cs
+++ src/Activities.Api/Program.cs
@@ -1,3 +1,4 @@
+using Activities.Infrastructure.Persistance;
 using Activities.Infrastructure.Persistance.Context;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,13 +7,30 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<ActivityContext>(options => {
+builder.Services.AddDbContext<ActivityContext>(options =>
+{
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<ActivityContext>();
+    await context.Database.MigrateAsync();
+    await DbInitializer.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration and seeding.");
+}
+
 
 app.Run();
