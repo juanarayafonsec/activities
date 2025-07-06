c24b24f3e18f194daa94f8f63875190b71d1507b
juanarayafonsec <juan-jose95@hotmail.com>
2025-07-06 15:27:47 +0200

    Add ActivitiesController and BaseApiController
    Introduces the `ActivitiesController` for managing activity-related HTTP requests, including endpoints for retrieving all activities and a specific activity by ID. Implements asynchronous data fetching using Entity Framework Core with `ActivityContext`.
    
    Also creates a `BaseApiController` to serve as a base for API controllers, equipped with routing and API-specific attributes. Additional using directives for ASP.NET Core MVC and Entity Framework Core are included.
    
    Note: Binary changes in `activities.db` indicate updates to the database file.

---
+++ src/Activities.Api/Controllers/ActivitiesController.cs
@@ -0,0 +1,25 @@
+﻿using Activities.Domain.Entity;
+using Activities.Infrastructure.Persistance.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Activities.Api.Controllers;
+public class ActivitiesController(ActivityContext context) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        return await context.Activities.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivity(string id)
+    {
+        var activity = await context.Activities.FindAsync(Guid.Parse(id));
+
+        if (activity == null) return NotFound();
+
+        return activity;
+    }
+}
+
---
+++ src/Activities.Api/Controllers/BaseApiController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Activities.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class BaseApiController : ControllerBase
+{
+}
+
