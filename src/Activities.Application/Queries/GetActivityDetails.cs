07c891e05f3f6fec633afec835f04ba83b9ed03f
juanarayafonsec <juan-jose95@hotmail.com>
2025-07-16 22:41:56 +0200

    Add query and handler classes for activity retrieval
    Introduce `GetActivityDetailsQuery` and `GetActivitiesQuery`
    with their respective handlers. Implement functionality to
    retrieve a single activity by ID and a list of all activities
    from the database. Update using directives for necessary
    namespaces.

---
+++ src/Activities.Application/Queries/GetActivityDetails.cs
@@ -0,0 +1,18 @@
+﻿using Activities.Application.Messaging;
+using Activities.Domain.Entity;
+using Activities.Infrastructure.Persistance.Context;
+
+
+namespace Activities.Application.Queries;
+
+public record GetActivityDetailsQuery(Guid Id) : IQuery<Activity>;
+
+
+public class GetActivityDetailsQueryHandler(ActivityContext context) : IQueryHandler<GetActivityDetailsQuery, Activity>
+{
+    public async Task<Activity> HandleAsync(GetActivityDetailsQuery query, CancellationToken cancellationToken)
+    {
+        var activity = await context.Activities.FindAsync([query.Id], cancellationToken);
+        return activity == null ? throw new Exception("Activity not found") : activity;
+    }
+}
---
+++ src/Activities.Application/Queries/GetActivityList.cs
@@ -0,0 +1,16 @@
+﻿using Activities.Application.Messaging;
+using Activities.Domain.Entity;
+using Activities.Infrastructure.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Activities.Application.Queries;
+public sealed class GetActivitiesQuery() : IQuery<List<Activity>>;
+
+
+public sealed class GetActivitiesQueryHandler(ActivityContext context) : IQueryHandler<GetActivitiesQuery, List<Activity>>
+{
+    public async Task<List<Activity>> HandleAsync(GetActivitiesQuery query, CancellationToken cancellationToken)
+    {
+        return await context.Activities.ToListAsync(cancellationToken);
+    }
+}
