a286512d616952fa425352390e0627fc07b2df11
juanarayafonsec <juan-jose95@hotmail.com>
2025-07-17 23:57:14 +0200

    Refactor App component structure and styles; remove unused files

---
+++ src/Activities.Frontend/src/app/layout/App.tsx
@@ -0,0 +1,29 @@
+import { List, ListItem, ListItemText, Typography } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import NavBar from "./NavBar";
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<Activity[]>("https://localhost:5001/api/activities")
+      .then((response) => setActivities(response.data));
+  }, []);
+
+  return (
+    <>
+      <NavBar />
+      <List>
+        {activities.map((activity) => (
+          <ListItem key={activity.id}>
+            <ListItemText>{activity.title}</ListItemText>
+          </ListItem>
+        ))}
+      </List>
+    </>
+  );
+}
+
+export default App;
