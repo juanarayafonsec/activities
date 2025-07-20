66c86044a6f43d756987ec64419ba2afdfd6b169
juanarayafonsec <juan-jose95@hotmail.com>
2025-07-20 02:05:59 +0200

    Add ActivityForm component for creating new activities in the dashboard

--- src/Activities.Frontend/src/features/activities/dashboard/ActivitiesDashboard.tsx
+++ src/Activities.Frontend/src/features/activities/dashboard/ActivitiesDashboard.tsx
@@ -1,6 +1,7 @@
 import { Grid2 } from "@mui/material";
 import ActivityList from "./ActivityList";
 import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
 
 type Props = {
   activities: Activity[];
@@ -22,6 +23,7 @@ export default function ActivitiesDashboard({
         </Grid2>
         <Grid2 size={5}>
           {selectedActivity && <ActivityDetails activity={selectedActivity} cancelSelectActivity={cancelSelectActivity} />}
+          <ActivityForm />
         </Grid2>
       </Grid2>
     </>
