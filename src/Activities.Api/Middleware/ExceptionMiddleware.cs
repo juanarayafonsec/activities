90cf449d6d6b29b52e0fe6522eee578c92a0e728
juanarayafonsec <juan-jose95@hotmail.com>
2025-08-13 07:56:04 +0200

    Exception middleware improvement

--- src/Activities.Api/Middleware/ExceptionMiddleware.cs
+++ src/Activities.Api/Middleware/ExceptionMiddleware.cs
@@ -1,7 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Activities.Api.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
 
 namespace Activities.Api.Middleware;
-public class ExceptionMiddleware : IMiddleware
+public class ExceptionMiddleware(ILogger<ExceptionMiddleware> logger, IHostEnvironment env) : IMiddleware
 {
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
@@ -11,7 +13,27 @@ public class ExceptionMiddleware : IMiddleware
         }
         catch (Exception ex)
         {
-            Console.WriteLine(ex.ToString());
+            await HandleException(context, ex);
         }
+    }
+
+    private async Task HandleException(HttpContext context, Exception ex)
+    {
+        logger.LogError(ex, ex.Message);
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+        var response = env.IsDevelopment()
+            ? new AppException(context.Response.StatusCode, ex.Message, ex.StackTrace)
+            : new AppException(context.Response.StatusCode, ex.Message, null);
+
+        var options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+
+        var json = JsonSerializer.Serialize(response, options);
+
+        await context.Response.WriteAsync(json);
     }
 }
