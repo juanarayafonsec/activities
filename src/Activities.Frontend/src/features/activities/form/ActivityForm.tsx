c9a576153114312867e48f2f53240bc832ddc082
juanarayafonsec <juan-jose95@hotmail.com>
2025-08-18 08:17:11 +0200

    Add form validation

--- src/Activities.Frontend/src/features/activities/form/ActivityForm.tsx
+++ src/Activities.Frontend/src/features/activities/form/ActivityForm.tsx
@@ -1,43 +1,37 @@
-import { Box, Button, Paper, TextField, Typography } from "@mui/material";
-import type { FormEvent } from "react";
+import { Box, Button, Paper, Typography } from "@mui/material";
 import { useActivities } from "../../../lib/hooks/useActivities";
 import { useNavigate, useParams } from "react-router";
-
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
+import {
+  activitySchema,
+  type ActivitySchema,
+} from "../../../lib/schemas/activitySchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import TextInput from "../../../app/shared/components/TextInput";
 
 export default function ActivityForm() {
-  const {id} = useParams();
-  const { updateActivity, createActivity, activity, isLoadingActivity } = useActivities(id);
+  const {register, reset, control, handleSubmit} = useForm<ActivitySchema>({
+    mode: "onTouched",
+    resolver: zodResolver(activitySchema),
+  });
+  const { id } = useParams();
+  const { updateActivity, createActivity, activity, isLoadingActivity } =
+    useActivities(id);
+
+  useEffect(() => {
+    if (activity) {
+      reset(activity);
+    }
+  }, [activity, reset]);
+
   const navigate = useNavigate();
 
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const formData = new FormData(event.currentTarget);
-
-    const data: { [key: string]: FormDataEntryValue } = {};
-
-    formData.forEach((value, key) => {
-      data[key] = value;
-    });
-
-    data.isCancelled = activity?.isCancelled ?? false;
-    data.latitude = activity?.latitude ?? 0;
-    data.longitude = activity?.longitude ?? 0;
-
-    if (activity) {
-      data.id = activity.id;
-      await updateActivity.mutateAsync(data as unknown as Activity);
-      navigate(`/activities/${activity.id}`);
-    } else {
-      createActivity.mutate(data as unknown as Activity, {
-        onSuccess: (id) => {
-          navigate(`/activities/${id}`);
-        }
-      });
-    }
+  const onSubmit = (data: ActivitySchema) => {
+    console.log(data);
   };
 
-  if(isLoadingActivity) {
+  if (isLoadingActivity) {
     return <Typography>Loading...</Typography>;
   }
 
@@ -47,19 +41,20 @@ export default function ActivityForm() {
         <Typography variant="h5" gutterBottom color="primary">
           {activity ? "Edit Activity" : "Create Activity"}
         </Typography>
-        <Box component="form" display="flex" flexDirection="column" gap={3} onSubmit={handleSubmit}>
-          <TextField name="title" label="Title" defaultValue={activity?.title} />
-          <TextField name="description" label="Description" multiline rows={3} defaultValue={activity?.description}/>
-          <TextField name="category" label="Category" defaultValue={activity?.category} />
-          <TextField
-            name="date"
-            label="Date"
-            type="date"
-            defaultValue={activity?.date ? new Date(activity.date).toISOString().split("T")[0] :new Date().toISOString().split("T")[0]}
-          />
-          <TextField name="city" label="City" defaultValue={activity?.city} />
-          <TextField name="venue" label="Venue" defaultValue={activity?.venue}
-          />
+        <Box
+          component="form"
+          display="flex"
+          flexDirection="column"
+          gap={3}
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <TextInput label="Title" name="title" control={control}/>
+          <TextInput label="Description" name="description" control={control} multiline rows={3}/>
+          <TextInput label="Category" name="category" control={control}/>
+          <TextInput label="Date" name="date" control={control}/>
+          <TextInput label="City" name="city" control={control}/>
+          <TextInput label="Venue" name="venue" control={control}/>
+          <TextInput label="City" name="city" control={control}/>    
           <Box display="flex" justifyContent="end" gap={3}>
             <Button color="inherit"> Cancel </Button>
             <Button
