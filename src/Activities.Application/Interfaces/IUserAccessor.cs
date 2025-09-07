77677f6956864ad3a0ad4e3923651d3c27efd7cd
juanarayafonsec <juan-jose95@hotmail.com>
2025-09-07 16:50:55 +0200

    Add missing file from previous commit. Add User Accessor

---
+++ src/Activities.Application/Interfaces/IUserAccessor.cs
@@ -0,0 +1,9 @@
+﻿using Activities.Domain.Entity;
+
+namespace Activities.Application.Interfaces;
+public interface IUserAccessor
+{
+    string GetUserId();
+    Task<User> GetUserAsync();
+}
+
---
+++ src/Activities.Infrastructure/Identity/UserAccessor.cs
@@ -0,0 +1,21 @@
+﻿using Activities.Application.Interfaces;
+using Activities.Domain.Entity;
+using Activities.Infrastructure.Persistance.Context;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace Activities.Infrastructure.Identity;
+public class UserAccessor(IHttpContextAccessor httpContextAccessor, ActivityContext context) : IUserAccessor
+{
+    public async Task<User> GetUserAsync()
+    {
+        return await context.Users.FindAsync(GetUserId())
+            ?? throw new Exception("User not found");
+    }
+
+    public string GetUserId()
+    {
+        return httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier)
+            ?? throw new Exception("User not found");
+    }
+}
